abb7c448ca6240ff4744da9a7bb5eb7b8460e081
fake-stmz <artur.kravchenko.2006@gmail.com>
2025-02-27 12:21:30 +0300

    second task done

--- task.py
+++ task.py
@@ -1,12 +1,18 @@
+import re # Импорт RegEx для проверки ограничений
+
 class PC: # Создание класса компьютера (системного блока)
 
     def __init__(self, motherboard, cpu, psu): # Конструктор класса
-        self.__motherboard = motherboard # Инициализация поля материнской платы
-        self.__cpu = cpu # Инициализация поля процессора
-        self.__psu = psu # Инициализация поля блока питания
+        self.set_motherboard(motherboard) # Инициализация поля материнской платы
+        self.set_cpu(cpu) # Инициализация поля процессора
+        self.set_psu(psu) # Инициализация поля блока питания
 
     # Для материнской платы
     def set_motherboard(self, motherboard): # set
+        if len(motherboard) < 20 or len(motherboard) > 150: # Проверка ограничения
+            # Вывод ошибки
+            print("[!] Название материнской платы должно быть не менее 20 символов и не более 150 символов")
+            return # Досрочное завершение функции
         self.__motherboard = motherboard # Присвоение нового значения поля
     
     def get_motherboard(self): # get
@@ -14,6 +20,10 @@ class PC: # Создание класса компьютера (системно
     
     # Для процессора
     def set_cpu(self, cpu): # set
+        if not re.fullmatch(r"[A-Za-z0-9 .;-]+", cpu): # Проверка ограничения
+            # Вывод ошибки
+            print("[!] Процессор должен содержать только буквы, цифры, тире, точки и/или точки с запятой")
+            return # Досрочное завершение функции
         self.__cpu = cpu # Присвоение нового значения поля
     
     def get_cpu(self): # get
@@ -21,6 +31,10 @@ class PC: # Создание класса компьютера (системно
     
     # Для блока питания
     def set_psu(self, psu): # set
+        if not re.fullmatch(r".*\d{3,4}W.*", psu): # Проверка ограничения
+            # Вывод ошибки
+            print("[!] Блок питания должен содержать значение напряжение в формате \"<Число>W\"")
+            return # Досрочное завершение функции
         self.__psu = psu # Присвоение нового значения поля
     
     def get_psu(self): # get
@@ -29,9 +43,9 @@ class PC: # Создание класса компьютера (системно
 # Создание объектов
 
 # Системный блок 1
-pc1 = PC("ASUS Z790", "AMD Threadripper 9999X", "KCAS 1500W Bomb Edition")
+pc1 = PC("ASUSSSSSSSSSSSSSSSSSSS Z790", "AMD Threadripper 9999X", "KCAS 1500W Bomb Edition")
 # Системный блок 2
-pc2 = PC("GIGABYTE B550", "Intel i5-10400F", "Thermaltake 600W")
+pc2 = PC("GIGABYTEEEEEEEEEEEE B550", "Intel i5-10400F", "Thermaltake 600W")
 
 # Вывод значений свойств объектов
 for pc in [pc1, pc2]: # Перебор компьютеров
@@ -39,3 +53,14 @@ for pc in [pc1, pc2]: # Перебор компьютеров
     print(f"Материнская плата: {pc.get_motherboard()}\n" +\
     f"Процессор: {pc.get_cpu()}\n" +\
     f"Блок питания: {pc.get_psu()}")
+
+# Проверка работы ограничений
+pc1.set_motherboard("0") # Материнская плата
+pc1.set_cpu("##@##@##@#") # Процессор 
+pc1.set_psu("No w no digits :(") # Блок питания
+
+# Замена данными, подходящими под ограничения
+pc2.set_motherboard("1" * 30) # Материнская плата
+pc2.set_cpu("Qualcomm 850-1X") # Процессор 
+pc2.set_psu("Omega 200W") # Блок питания
+print(pc2.get_motherboard(), pc2.get_cpu(), pc2.get_psu()) # Вывод свойств
